fc7744bc1394b66661b7393c597f11729484ac32
Pietro <92267915+pietrosprudente@users.noreply.github.com>
2024-10-13 11:42:59 +0100

    Add files via upload

---
+++ LightningFastPunExtension.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System;
+using System.Threading.Tasks;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Photon.Pun {
+
+    public class ServerEvent : PunRPC {
+    }
+
+    public class LightningFastPUN : MonoBehaviour {
+        // RPC
+        static PhotonView selectedPhotonView;
+
+        public static async Task
+            PerformRpcDelay(float delay) {
+            await Task.Delay(Mathf.RoundToInt(delay) * 1000);
+        }
+
+        public static async void SendServerEvent(string voidName, bool secureRpc, PhotonView view, float time, RpcTarget targets, params object[] parameters) {
+            Debug.DebugBreak();
+            PhotonNetwork.RunRpcCoroutines = true;
+            await PerformRpcDelay(time);
+            LightningFastPUN lightningFastPUN = new LightningFastPUN();
+            lightningFastPUN.ServerSendEventWait(voidName, secureRpc, view, targets, parameters);
+        }
+
+        private void ServerSendEventWait(string voidName, bool secureRpc, PhotonView view, RpcTarget targets, params object[] parameters) {
+            selectedPhotonView = view;
+            if (secureRpc) {
+                selectedPhotonView.RpcSecure(voidName, targets, true, parameters);
+            }
+            else {
+                selectedPhotonView.RPC(voidName, targets, parameters);
+            }
+        }
+    }
+}
