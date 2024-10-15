a9e174feee0eaf5d108ac821aef13d03021606f1
Pietro <92267915+pietrosprudente@users.noreply.github.com>
2024-10-15 20:01:31 +0100

    Update LightningFastPunExtension.cs

--- LightningFastPunExtension.cs
+++ LightningFastPunExtension.cs
@@ -1,10 +1,7 @@
 using UnityEngine;
 using System;
 using System.Threading.Tasks;
-using UnityEditor;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace Photon.Pun {
 
