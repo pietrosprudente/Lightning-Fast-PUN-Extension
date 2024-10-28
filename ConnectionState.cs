3e9710a20a2086951bab4efa35a68e51f4f374a4
Pietro <92267915+pietrosprudente@users.noreply.github.com>
2024-10-28 08:44:09 +0000

    Create ConnectionState.cs

---
+++ ConnectionState.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class ConnectionState : MonoBehaviourPunCallbacks
+{
+    public TMP_Text[] connectionTexts;
+    public string connectionState;
+
+    // For Mindless Physics ;)
+    public Toggle OnlineToggle;
+
+    public override void OnConnectedToMaster() {
+        connectionState = "Connected To Master";
+        ChangeConnectionTexts(Color.green);
+        base.OnConnected();
+    }
+
+    public override void OnCreatedRoom() {
+        connectionState = "Created Room";
+        ChangeConnectionTexts(Color.green);
+        base.OnCreatedRoom();
+    }
+
+    public override void OnJoinedRoom() {
+        connectionState = "Joined Room";
+        ChangeConnectionTexts(Color.green);
+        base.OnJoinedRoom();
+    }
+
+    public override void OnLeftRoom() {
+        connectionState = "Left Room";
+        ChangeConnectionTexts(Color.red);
+        base.OnLeftRoom();
+    }
+
+    public override void OnErrorInfo(ErrorInfo errorInfo) {
+        connectionState = $"Error: {errorInfo}";
+        base.OnErrorInfo(errorInfo);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) {
+        connectionState = $"Creating Room Failed, Return Code: {returnCode}, Message: {message}";
+        ChangeConnectionTexts(Color.red);
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+        connectionState = $"Disconnected, Cause: {cause}";
+        ChangeConnectionTexts(Color.red);
+        base.OnDisconnected(cause);
+    }
+
+    public override void OnConnected() {
+        connectionState = "Connected";
+        ChangeConnectionTexts(Color.green);
+        base.OnConnected();
+    }
+
+
+    private void Start() {
+        if (PhotonNetwork.IsConnected) {
+            connectionState = "Online";
+        }
+        else
+        {
+            connectionState = "Offline";
+        }
+    }
+
+    private void ChangeConnectionTexts(Color newColor) {
+        if (OnlineToggle != null) {
+            if (PhotonNetwork.IsConnected) {
+                OnlineToggle.onValueChanged.Invoke(true);
+            }
+            else {
+                OnlineToggle.onValueChanged.Invoke(false);
+            }
+        }    
+        if (connectionTexts == null)
+            return;
+        foreach (var text in connectionTexts) {
+            text.text = connectionState;
+            text.color = newColor;
+        }
+    }
+}
