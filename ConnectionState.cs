b5b42869c93c53b6f5b85b2a47cb88444ff698d8
Pietro <92267915+pietrosprudente@users.noreply.github.com>
2024-10-29 09:28:17 +0000

    Update ConnectionState.cs

--- ConnectionState.cs
+++ ConnectionState.cs
@@ -59,11 +59,11 @@ public class ConnectionState : MonoBehaviourPunCallbacks
 
     private void Start() {
         if (PhotonNetwork.IsConnected) {
-            connectionState = "Online";
+            connectionState = "Connected";
         }
         else
         {
-            connectionState = "Offline";
+            connectionState = "Not Connected";
         }
     }
 
