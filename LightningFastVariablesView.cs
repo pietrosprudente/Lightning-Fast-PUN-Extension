ebafe7858a18f60683d1d0853291a86050634116
Pietro <92267915+pietrosprudente@users.noreply.github.com>
2024-10-29 09:15:56 +0000

    New Way of Using Variables View!

--- LightningFastVariablesView.cs
+++ LightningFastVariablesView.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class LightningFastVariablesView : MonoBehaviourPunCallbacks, IPunObservable {
+public class LightningFastVariablesView : MonoBehaviour, IPunObservable {
+
+    private PhotonView photonView;
+
     private object[] localStoredVariables = {
         0,
         1,
@@ -26,12 +29,17 @@ public class LightningFastVariablesView : MonoBehaviourPunCallbacks, IPunObserva
 
     private List<object> localStoredVariablesList;
 
+    void Start() { 
+        photonView = GetComponent<PhotonView>();
+    }
+
     /// <summary>
-    /// Can only Save from 0 to 16 variables
+    /// Can only Save from 0 to 16 variables!
     /// </summary>
     /// <param name="index"> the index stored that the value is stored</param>
     /// <param name="value"> the value the index is stored in</param>
     public void SetCloudVariable(int index, object value) {
+        if(value == localStoredVariables.GetValue(index)) return;
         localStoredVariables.SetValue(value, index);
         localStoredVariablesList = localStoredVariables.ToList();
         for (int i = 0; i < localStoredVariables.Length; i++) {
@@ -45,15 +53,35 @@ public class LightningFastVariablesView : MonoBehaviourPunCallbacks, IPunObserva
     }
 
     /// <summary>
-    /// Can only Load from 0 to 16 variables
+    /// Can only Load from 0 to 16 variables!
     /// </summary>
     /// <param name="index"> the index stored that the value is stored</param>
     public object GetCloudVariable(int index) {
-        if (localStoredVariables.GetValue(index) == null)
-            return null;
-        object value;
-        value = localStoredVariables.GetValue(index);
-        return value;
+        if (photonView.IsMine)
+            return localStoredVariables.GetValue(index);
+        else {
+            if (localStoredVariables.GetValue(index) == null)
+                return null;
+            return localStoredVariables.GetValue(index);
+        }        
+    }
+
+    /// <summary>
+    /// Can Save and Load from 0 to 16 variables!
+    /// </summary>
+    /// <param name="index"> the index stored that the value is stored</param>
+    /// <param name="value"> the value the index is stored in</param>
+    public object CloudVariable(int index, object value = null) {
+        if (photonView.IsMine) {
+            if (value != null && value != GetCloudVariable(index)) {
+                SetCloudVariable(index, value);
+            }
+            return localStoredVariables.GetValue(index);
+        }
+        else if (GetCloudVariable(index) != null){
+            return GetCloudVariable(index);
+        }      
+        return null;
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
@@ -64,13 +92,19 @@ public class LightningFastVariablesView : MonoBehaviourPunCallbacks, IPunObserva
         }
         else {
             for (int i = 0; i < localStoredVariables.Length; i++) {
-                localStoredVariables.SetValue((object)stream.ReceiveNext(), i);
+                localStoredVariables.SetValue(stream.ReceiveNext(), i);
                 localStoredVariablesList = localStoredVariables.ToList();
                 if (localStoredVariables[i] == null) {
                     localStoredVariablesList.Remove(i);
                     localStoredVariables = localStoredVariablesList.ToArray();
                 }
             }
+        }
+    }
+
+    private void PrintConsole(object message) {
+        if (PhotonNetwork.PhotonServerSettings.PunLogging >= (PunLogLevel)1) {
+            Debug.Log(message);
         }
     }
 }
